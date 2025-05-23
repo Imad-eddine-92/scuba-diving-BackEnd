c433edd66cfd31b31bddcc45324136e6e882f9cc
IMAD <shevchenko_imad@live.fr>
2025-05-23 22:32:13 +0100

    Ignore client folder

--- client/src/JS/actionTypes/bookActionType.js
+++
@@ -1,8 +0,0 @@
-
-export const LOAD_BOOK = 'LOAD_BOOK';
-export const GET_BOOKS = 'GET_BOOKS';
-export const GET_ONE_BOOK = 'GET_ONE_BOOK';
-export const GET_MY_BOOKS = 'GET_MY_BOOKS';
-export const ADD_BOOK = 'ADD_BOOK';
-export const DELETE_BOOK = 'DELETE_BOOK';
-export const FAIL_BOOK = 'FAIL_BOOK';
